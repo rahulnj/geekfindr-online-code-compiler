473fc181c7cabd8b46bc246ba1c7e75ff496e3c8
Rahul <rahuljayaraman8@gmail.com>
2022-01-30 12:43:09 +0530

    added the funtionality to fetch data from the nested files

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -12,18 +12,18 @@ export const unpkgPathPlugin = () => {
                     return { path: args.path, namespace: 'a' };
                 }
 
+                if (args.path.includes('./') || args.path.includes('../')) {
+                    return {
+                        namespace: 'a',
+                        path: new URL(args.path,
+                            'https://unpkg.com' + args.resolveDir + '/').href,
+                    };
+                }
+
                 return {
                     namespace: 'a',
                     path: `https://unpkg.com/${args.path}`
                 }
-
-
-                //  else if (args.path === 'tiny-test-pkg') {
-                //     return {
-                //         path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-                //         namespace: 'a'
-                //     }
-                // }
             });
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -33,15 +33,17 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: `
-              import message from 'tiny-test-pkg';
-              console.log(message);
+                     import React,{useState} from 'react'
+              console.log(useState);
             `,
                     };
                 }
-                const { data } = await axios.get(args.path);
+                const { data, request } = await axios.get(args.path);
+                console.log(request);
                 return {
                     loader: 'jsx',
                     contents: data,
+                    resolveDir: new URL('./', request.responseURL).pathname
                 }
 
             });
