5ab40d5286c4186552c079eb2000ff7762fb9415
Rahul <rahuljayaraman8@gmail.com>
2022-03-12 23:14:35 +0530

    changes in the style in the codecell and useEffect initial render fixed

--- src/components/CodeCell/CodeCell.tsx
+++ src/components/CodeCell/CodeCell.tsx
@@ -22,6 +22,10 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const bundle = useTypedSelector((state: any) => state.bundles[cell.id])
 
     useEffect(() => {
+        if (!bundle) {
+            createBundle(cell.id, cell.content)
+            return;
+        }
         const timer = setTimeout(async () => {
             createBundle(cell.id, cell.content)
         }, 750);
@@ -44,8 +48,18 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
                         onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
-                {bundle && <Preview code={bundle.code} err={bundle.err} />}
-            </div >
+                <div className="progress-wrapper">
+                    {!bundle || bundle.loading ? (
+                        <div className='progress-cover'>
+                            <progress className='progress is-small is-primary' max="100">
+                                Loading
+                            </progress>
+                        </div>
+                    ) : (
+                        <Preview code={bundle.code} err={bundle.err} />
+                    )}
+                </div>
+            </div>
         </Resizable>
     )
 
